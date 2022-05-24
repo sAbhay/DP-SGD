df7bcc2e487522f95324de97f15a6fe9ec55c193
Abhay Singhal <abhayaaditsinghal@gmail.com>
2022-05-24 12:40:15 -0700

    plotting logits

--- jax/analysis.py
+++ jax/analysis.py
@@ -13,8 +13,11 @@ for i, norm in enumerate(norms):
     norms[i] = (norm[0], norm[1].item(), norm[2].item(), *norm[3])
 
 cols = ['epoch', 'norm', 'accurate']
-cols += [f'{i}_logit' for i in range(0, 10)]
-df = pd.DataFrame(norms, columns=cols)
+logit_cols = [f'{i}_logit' for i in range(0, 10)]
+df = pd.DataFrame(norms, columns=cols+logit_cols)
+df['logits_stddev'] = df[logit_cols].std(axis=1)
 # print(len(df[df['epoch']==0]), len(df[(df['epoch']==0) & (df['accurate']==0)]), len(df[(df['epoch']==0) & (df['accurate']==1)]), len(df[df['epoch']==10]))
 ax = df.hist(column=['norm'], by=['epoch'], sharey=True, sharex=True)
 plt.show()
+
+# sns.scatterplot(x=df['index'], y=df['data'], c=df['color'], style=df['marker'])
