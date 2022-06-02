33d3a8468520cb8095c603cdf4f8080a7a1efab3
Abhay Singhal <abhayaaditsinghal@gmail.com>
2022-06-01 23:45:53 -0700

    updated analysis for hyperparameter specification

--- jax/analysis.py
+++ jax/analysis.py
@@ -3,8 +3,9 @@ import pickle
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+hyperparams_string = "dpsgd_loss=cross-entropy,lr=0.25,op=True,nm=1.3,l2nc=1.5"
 
-with open('grad_norms_dpsgd.pkl', 'rb') as f:
+with open(rf'norms\grad_norms_{hyperparams_string}.pkl', 'rb') as f:
     epoch_norms = pickle.load(f)
 # print(epoch_norms[0])
 
@@ -15,7 +16,7 @@ for i, norm in enumerate(norms):
     norms[i] = tuple([norm[0], norm[1].item(), norm[2].item()] + list(norm[3]))
 # print(norms[0])
 
-with open('param_norms_dpsgd.pkl', 'rb') as f:
+with open(rf'norms\param_norms_{hyperparams_string}.pkl', 'rb') as f:
     param_norms = pickle.load(f)
 for i, param_norm in enumerate(param_norms):
     param_norms[i] = (i, param_norm.item())
@@ -29,11 +30,10 @@ sample_df['logits_stddev'] = sample_df[logit_cols].std(axis=1)
 
 # print(len(df[df['epoch']==0]), len(df[(df['epoch']==0) & (df['accurate']==0)]), len(df[(df['epoch']==0) & (df['accurate']==1)]), len(df[df['epoch']==10]))
 # ax = df.hist(column=['norm'], by=['epoch'], sharey=True, sharex=True)
+sample_df[sample_df['epoch'] == 19].hist(column='norm', by='accurate', legend=False)
+
 ax = sns.histplot(data=sample_df, x='epoch', y='norm', hue='accurate')
 sns.move_legend(ax, "upper left")
-plt.show()
-
-sns.scatterplot(data=sample_df, x='norm', y='logits_stddev', hue='epoch', style='accurate')
 plt.show()
 
 cols = ['epoch', 'param_norm']
@@ -41,4 +41,7 @@ epoch_df = pd.DataFrame(param_norms, columns=cols)
 epoch_df['expected_grad_norm'] = sample_df.groupby(['epoch']).mean()['norm']
 sns.lineplot(x='epoch', y='value', hue='variable',
              data=pd.melt(epoch_df[['epoch', 'param_norm', 'expected_grad_norm']], ['epoch']))
+plt.show()
+
+sns.scatterplot(data=sample_df, x='norm', y='logits_stddev', hue='epoch', style='accurate')
 plt.show()
