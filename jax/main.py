6dc053bd1c9f75b69a8248e16768baefde91f50d
Abhay Singhal <abhayaaditsinghal@gmail.com>
2022-07-06 16:00:12 -0700

    commented logging

--- jax/main.py
+++ jax/main.py
@@ -281,7 +281,7 @@ def main(_):
         params = get_params(opt_state)
         avg_params = average_params(params, add_params, i)
         # logger.info(f"Average params: {avg_params}, \n Grads: {private_grads}")
-        logger.info("Optimization state: {}".format(opt_state))
+        # logger.info("Optimization state: {}".format(opt_state))
         opt_state = set_params(private_grads, opt_state)
         return opt_state, total_grad_norm
 
