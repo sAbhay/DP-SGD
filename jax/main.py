7e8a707cf662aad3f7be6231a5bbfe5dd2fb5c59
Abhay Singhal <abhayaaditsinghal@gmail.com>
2022-07-07 14:08:24 -0700

    unlogging grad norm

--- jax/main.py
+++ jax/main.py
@@ -390,7 +390,7 @@ def main(_):
             else:
                 hyperparams_string = f"{'dpsgd' if FLAGS.dpsgd else 'sgd'}_loss={FLAGS.loss},lr={FLAGS.learning_rate},op={FLAGS.overparameterised},nm={FLAGS.noise_multiplier},l2nc={FLAGS.l2_norm_clip},grp={FLAGS.groups},bs={FLAGS.batch_size},ws={FLAGS.weight_standardisation},mu={FLAGS.ema_coef},ess={FLAGS.ema_start_step},pss={FLAGS.polyak_start_step},pa={FLAGS.param_averaging},aug={FLAGS.augmult},rf={FLAGS.random_flip},rc={FLAGS.random_crop}"
             with open(f'grad_norms_{hyperparams_string}.pkl', 'wb') as f:
-                logger.info("grad norms: {}".format(grad_norms[-1][0:100]))
+                # logger.info("grad norms: {}".format(grad_norms[-1][0:100]))
                 pickle.dump(grad_norms, f)
             with open(f'param_norms_{hyperparams_string}.pkl', 'wb') as f:
                 # logger.info("param norms: {}".format(param_norms[-1]))
