427f45272a2a24de0ee2fab36cee48e259c53e7c
Abhay Singhal <abhayaaditsinghal@gmail.com>
2022-07-22 00:04:04 -0700

    added no group support for CNN

--- jax/models/mnist.py
+++ jax/models/mnist.py
@@ -24,4 +24,21 @@ def get_mnist_model_fn(overparameterised=True, groups=8):
         ])(features)
         return out
 
+    def mnist_model_fn_no_group(features, **_):
+        out = hk.Sequential([
+            WSConv2D(16*mult, (8, 8), padding='SAME', stride=(2, 2)),
+            jax.nn.relu,
+            hk.MaxPool(2, 1, padding='VALID'),  # matches stax
+            WSConv2D(32*mult, (4, 4), padding='VALID', stride=(2, 2)),
+            jax.nn.relu,
+            hk.MaxPool(2, 1, padding='VALID'),  # matches stax
+            hk.Flatten(),
+            hk.Linear(32),
+            jax.nn.relu,
+            hk.Linear(10),
+        ])(features)
+        return out
+
+    if groups == 0:
+        return mnist_model_fn_no_group
     return mnist_model_fn
