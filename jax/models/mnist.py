ff8756aa835613e5b5792828daa713b30dcfdcc9
Abhay Singhal <abhayaaditsinghal@gmail.com>
2022-07-25 01:34:30 -0700

    returning CNN seq

--- jax/models/mnist.py
+++ jax/models/mnist.py
@@ -55,4 +55,4 @@ def get_mnist_model_fn(overparameterised=True, groups=8, weight_standardisation=
         layers.append(hk.Linear(output_classes))
         model = hk.Sequential(layers)
         return model(features)
-    return mnist_model_fn
+    return mnist_model_fn_seq
