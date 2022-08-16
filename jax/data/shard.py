5bafc3d188dc15545b19ced2d4def4b50b7ff4ee
Abhay Singhal <abhayaaditsinghal@gmail.com>
2022-08-16 12:57:49 -0700

    sharding dataset

--- jax/data/shard.py
+++ jax/data/shard.py
@@ -1,11 +1,11 @@
 import jax.numpy as jnp
-from jax import put_device_sharded, devices
+from jax import device_put_sharded
 
 
 def shard_dataset(images, labels, devices):
     """Split a dataset into num_shards shards."""
     images = jnp.split(images, len(devices))
     labels = jnp.split(labels, len(devices))
-    images = put_device_sharded(images, devices)
-    labels = put_device_sharded(labels, devices)
+    images = device_put_sharded(images, devices)
+    labels = device_put_sharded(labels, devices)
     return images, labels
