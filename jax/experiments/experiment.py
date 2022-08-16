837eb2e640dd25b31744ca02412fd9791eff8687
Abhay Singhal <abhayaaditsinghal@gmail.com>
2022-08-16 00:24:13 -0700

    debugging parallelisation

--- jax/experiments/experiment.py
+++ jax/experiments/experiment.py
@@ -436,7 +436,7 @@ def experiment():
         logger.info('Test set loss, accuracy (%): ({:.2f}, {:.2f})'.format(
             test_loss, 100 * test_acc))
         # log_memory_usage(logger, handle)
-        train_acc, _, _ = accuracy(params, shape_as_image(train_images, train_labels, augmult=0), splits=5)
+        train_acc, _, _ = accuracy(params, shape_as_image(train_images, train_labels, augmult=0), splits=4)
         # train_loss = loss(params, shape_as_image(train_images, train_labels, augmult=0))
         train_loss = test_loss
         logger.info('Train set loss, accuracy (%): ({:.2f}, {:.2f})'.format(
