2e63675f9d803ed98dbf60bf038779aa2738834f
Abhay Singhal <abhayaaditsinghal@gmail.com>
2022-08-15 17:19:58 -0700

    debugging parallelisation

--- jax/experiments/experiment.py
+++ jax/experiments/experiment.py
@@ -249,9 +249,9 @@ def experiment():
 
     def predict(params, inputs):
         if FLAGS.model == "cnn":
-            predictions = pmap(model.apply, axis_name='i')(params, None, inputs)
+            predictions = pmap(model.apply, axis_name='i')(params, inputs)
         elif FLAGS.model == "wideresnet" or FLAGS.model == "wideresnet2":
-            predictions = pmap(model.apply, axis_name='i')(params, None, inputs)
+            predictions = pmap(model.apply, axis_name='i')(params, inputs)
         else:
             return ValueError(f"Unknown model: {FLAGS.model}")
         return predictions
