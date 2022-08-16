d04be91b2fde894fccd5d33bd8d8804f5760c441
Abhay Singhal <abhayaaditsinghal@gmail.com>
2022-08-16 13:47:41 -0700

    Revert "debugging parallelisation"
    This reverts commit 03327d4f008e0c2886bbc0d1b93ef18a9cd61531.

--- jax/experiments/experiment.py
+++ jax/experiments/experiment.py
@@ -239,7 +239,7 @@ def experiment():
     model_fn = models.get_model_fn(FLAGS.model, model_kwargs)
     model = hk.transform(model_fn, apply_rng=True)
 
-    init_batch = (jnp.zeros((FLAGS.batch_size, *datasets.IMAGE_SHAPE[FLAGS.dataset])), jnp.zeros(train_labels[0:FLAGS.batch_size].shape))[0]
+    init_batch = (jnp.zeros((FLAGS.batch_size, *datasets.IMAGE_SHAPE[FLAGS.dataset])), jnp.zeros(train_labels[0:FLAGS.batch_size].shape))
     # init_batch = shape_as_image(*dummy_batch, dummy_dim=False, augmult=FLAGS.augmult, flatten_augmult=True)[0]
     logger.info(f"Init batch shape: {init_batch.shape}")
     init_args = [init_batch]
