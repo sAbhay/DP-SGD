68170f40faa795b5149792b6be2c9c7db5b78a6d
Abhay Singhal <abhayaaditsinghal@gmail.com>
2022-11-30 17:50:23 -0800

    debugging imports

--- fine-tune/experiments/image_classification/run_experiment.py
+++
@@ -1,4 +0,0 @@
-from src.training.image_classification import experiment
-
-if __name__ == "__main__":
-    experiment.run_experiment()
---
+++ fine_tune/experiments/image_classification/run_experiment.py
@@ -0,0 +1,4 @@
+from fine_tune.src.training.image_classification import experiment
+
+if __name__ == "__main__":
+    experiment.run_experiment()
