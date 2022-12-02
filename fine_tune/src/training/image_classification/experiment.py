8776681ac1fd3b861e86f6f6550c624b49664a3f
Abhay Singhal <abhayaaditsinghal@gmail.com>
2022-12-02 14:13:25 -0800

    sanity check one model

--- fine_tune/src/training/image_classification/experiment.py
+++ fine_tune/src/training/image_classification/experiment.py
@@ -16,6 +16,6 @@ def run_experiment():
   print(f"Dataset sizes: train: {len(trainset)}, val: {len(valset)}, test: {len(testset)}")
 
   model = train(trainset=trainset, model=model, loss_fn=loss, optimizer_fn=optimizer_fn,
-                         epochs=1, splits=1, batch_size=2048, max_steps=20, valset=valset)
+                         epochs=20, splits=1, batch_size=2048, max_steps=1, valset=valset)
   print(f"Final train loss: {total_loss(model, loss, trainset)}, accuracy: {accuracy(model, trainset)}")
   print(f"Final val loss: {total_loss(model, loss, valset)}, accuracy: {accuracy(model, valset)}")
