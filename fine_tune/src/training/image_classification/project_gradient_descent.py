23fec303f388ca676ba220dad8c62c18528f71d7
Abhay Singhal <abhayaaditsinghal@gmail.com>
2022-12-05 21:19:53 -0800

    debugging interpolation

--- fine_tune/src/training/image_classification/project_gradient_descent.py
+++ fine_tune/src/training/image_classification/project_gradient_descent.py
@@ -43,7 +43,7 @@ def interpolate_model(model, model_ref, max_dist):
 
   print("Interpolating", dist, max_dist)
   for key in sd:
-    sd[key] = sd_ref[key] + (sd[key] - sd_ref[key]) * (max_dist / (dist * len(sd)))
+    sd[key] = sd_ref[key] + (sd[key] - sd_ref[key]) * (max_dist / dist)
 
   model.load_state_dict(sd)
   return model
