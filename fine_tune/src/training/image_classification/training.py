852589c2d12d4b0664a8ff664b2e0013bbae37ab
Abhay Singhal <abhayaaditsinghal@gmail.com>
2022-12-05 19:59:13 -0800

    debugging interpolation

--- fine_tune/src/training/image_classification/training.py
+++ fine_tune/src/training/image_classification/training.py
@@ -31,8 +31,9 @@ def sub_train_loop(trainloader, model, loss_fn, optimizer, max_steps, model_ref=
 
       with torch.no_grad():
         if model_ref is not None and max_dist is not None:
-          model = interpolate_model(model, model_ref, max_dist)
-          print("Model dist = {}, step {}".format(model_dist(model, model_ref), max_dist), step)
+          int_model = interpolate_model(model, model_ref, max_dist)
+          print("Int model dist = {}, step {}".format(model_dist(int_model, model), step), max_dist)
+          print("Model dist = {}, step {}".format(model_dist(model, model_ref), step), max_dist)
 
       # print statistics
       running_loss += loss.item()
